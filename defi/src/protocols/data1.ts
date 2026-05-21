d87173cabb7066fbcf3c8f97da9e4b4278700c87
Real Shaman <85087525+realdealshaman@users.noreply.github.com>
2026-05-21 14:31:11 -0600

    Add startDate and endDate to oraclesBreakdown

--- defi/src/protocols/data1.ts
+++ defi/src/protocols/data1.ts
@@ -8035,8 +8035,10 @@ The eWIT token is a custodial, wrapped version of the Witnet coin managed by the
     twitter: "BenqiFinance",
     audit_links: ["https://docs.benqi.fi/risks#audits"],
     oraclesBreakdown: [
-      { name: "Chaos", type: "Secondary", proof: ["https://docs.benqi.fi/resources/contracts/price-feeds", "https://x.com/BenqiFinance/status/2052625080916558326"] },
-      { name: "Chainlink", type: "Primary", proof: ["https://docs.benqi.fi/resources/contracts/price-feeds", "https://x.com/BenqiFinance/status/2052625080916558326"] }
+      { name: "Chaos", type: "Secondary", proof: ["https://docs.benqi.fi/resources/contracts/price-feeds", "https://x.com/BenqiFinance/status/2052625080916558326"],
+      endDate: "2026-05-04"},
+      { name: "Chainlink", type: "Primary", proof: ["https://docs.benqi.fi/resources/contracts/price-feeds", "https://x.com/BenqiFinance/status/2052625080916558326"],
+      startDate: "2026-05-04}
     ],
     forkedFromIds: ["114"],
     parentProtocol: "parent#benqi",
